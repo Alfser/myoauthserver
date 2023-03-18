3761a48a8472f990c87bacbccd0552667e4903d5
janilson.serra <j.janilson12@gmail.com>
2023-03-17 22:08:55 -0300

    adição do projeto do servidor de autenticação.

---
+++ src/main/java/com/example/myoauthserver/MyoauthserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.myoauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyoauthserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyoauthserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myoauthserver/MyoauthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myoauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyoauthserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
