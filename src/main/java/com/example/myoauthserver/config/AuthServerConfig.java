57fd55f1523351dfbfc053e2860a5812d58a5092
janilson.serra <j.janilson12@gmail.com>
2023-03-19 15:10:22 -0300

    customizando os claims do token.

--- src/main/java/com/example/myoauthserver/config/AuthServerConfig.java
+++ src/main/java/com/example/myoauthserver/config/AuthServerConfig.java
@@ -1,15 +1,17 @@
 package com.example.myoauthserver.config;
 
+import java.util.function.Function;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationConsentService;
 import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService;
@@ -19,9 +21,13 @@ import org.springframework.security.oauth2.server.authorization.client.JdbcRegis
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.oidc.authentication.OidcUserInfoAuthenticationContext;
+import org.springframework.security.oauth2.server.authorization.oidc.authentication.OidcUserInfoAuthenticationToken;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import com.example.myoauthserver.config.keys.KeyManager;
 import com.example.myoauthserver.config.properties.AuthProperties;
@@ -41,18 +47,40 @@ public class AuthServerConfig {
     @Bean
 	@Order(Ordered.HIGHEST_PRECEDENCE)
 	public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
-		OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
-		http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
-			.oidc(Customizer.withDefaults());	// Enable OpenID Connect 1.0
+		OAuth2AuthorizationServerConfigurer authorizationServerConfigurer =
+				new OAuth2AuthorizationServerConfigurer();
+		RequestMatcher endpointsMatcher = authorizationServerConfigurer
+				.getEndpointsMatcher();
+
+		Function<OidcUserInfoAuthenticationContext, OidcUserInfo> userInfoMapper = (context) -> { 
+			OidcUserInfoAuthenticationToken authentication = context.getAuthentication();
+			JwtAuthenticationToken principal = (JwtAuthenticationToken) authentication.getPrincipal();
+
+			return new OidcUserInfo(principal.getToken().getClaims());
+		};
+
+		authorizationServerConfigurer
+			.oidc((oidc) -> oidc
+				.userInfoEndpoint((userInfo) -> userInfo
+					.userInfoMapper(userInfoMapper) 
+				)
+			);
+
 		http
+			.securityMatcher(endpointsMatcher)
+			.authorizeHttpRequests((authorize) -> authorize
+				.anyRequest().authenticated()
+			)
+			.csrf(csrf -> csrf.ignoringRequestMatchers(endpointsMatcher))
 			.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt) 
 			.exceptionHandling((exceptions) -> exceptions
 				.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
-			);
+			)
+			.apply(authorizationServerConfigurer); 
 
 		return http.build();
 	}
-	
+
     @Bean
 	public RegisteredClientRepository registeredClientRepository(PasswordEncoder encoder, JdbcTemplate jdbcTemplate) {
 		// RegisteredClient defaultClient = RegisteredClient.withId(UUID.randomUUID().toString())
@@ -138,5 +166,4 @@ public class AuthServerConfig {
 	public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
 		return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
 	}	
-
 }
--- src/main/java/com/example/myoauthserver/config/TokenCustomizerConfig.java
+++ src/main/java/com/example/myoauthserver/config/TokenCustomizerConfig.java
@@ -4,13 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
 import org.springframework.security.oauth2.core.oidc.endpoint.OidcParameterNames;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 
 import com.example.myoauthserver.service.OidcUserInfoService;
 
 @Configuration
-public class IdTokenCustomizerConfig {
+public class TokenCustomizerConfig {
 
 	@Bean 
 	public OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer(
@@ -21,6 +22,12 @@ public class IdTokenCustomizerConfig {
 						context.getPrincipal().getName());
 				context.getClaims().claims(claims ->
 						claims.putAll(userInfo.getClaims()));
+			}
+
+			if (OAuth2TokenType.ACCESS_TOKEN.equals(context.getTokenType())) {
+				context.getClaims().claims((claims) -> {
+					claims.put("authorities", context.getPrincipal().getAuthorities());
+				});
 			}
 		};
 	}
